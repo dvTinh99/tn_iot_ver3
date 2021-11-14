f3089dee7df1847976432c849c3529059454c8fe
tinh.doan <tinh5969@gmail.com>
2021-11-14 11:34:16 +0700

    edit index

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import ButtomComponent from './src/Components/ButtomComponent';
 import Tabs from './src/Components/Tabs';
 import {name as appName} from './app.json';
 
